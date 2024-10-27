4dbf88453e12ee6a10ada0d2d214a477861e8267
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2024-10-27 12:58:58 -0300

    ajustes finos na landing page

---
+++ scriptLandingPage.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const menuIcon = document.querySelector('.menu-icon');
+  const navbarMobile = document.querySelector('.navbar-mobile');
+
+  menuIcon.addEventListener('click', function() {
+      navbarMobile.classList.toggle('active');
+  });
+});
